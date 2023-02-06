5cb6779b8edf0a817d6baf61f5b59cc18b70a575
Ahmad Said <47715383+Ahmad-Said@users.noreply.github.com>
2023-02-06 13:55:54 +0100

    add simple sign in

---
+++ 1.HTML_CSS_Ahmad/users.php
@@ -0,0 +1,70 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css"
+          integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css"
+          integrity="sha512-+4zCK9k+qNFUR5X+cKL9EIR+ZOhtIloNl9GIKS57V1MyNsYpYcUrUeQc9vNfzsWfV28IaLL3i96P9sdNyeRssA=="
+          crossorigin="anonymous"/>
+    <title>Users</title>
+</head>
+<body>
+<div class="container">
+    <table class="table">
+        <caption>List of users</caption>
+        <thead>
+        <tr>
+            <th>User #</th>
+            <th>FirstName</th>
+            <th>LastName</th>
+            <th>Birthdate</th>
+            <th>Actions</th>
+        </tr>
+        </thead>
+        <tbody>
+        <tr>
+            <td>1</td>
+            <td>John</td>
+            <td>Doe</td>
+            <td>01/01/1980</td>
+            <td>
+                <a href="#">
+                    <i class="fas fa-edit"></i>
+                </a>
+                <a href="#">
+                    <i class="fas fa-trash-alt"></i>
+                </a>
+            </td>
+        </tr>
+        <tr>
+            <td>2</td>
+            <td>Jane</td>
+            <td>Doe</td>
+            <td>02/01/1985</td>
+            <td>
+                <a href="#">
+                    <i class="fas fa-edit"></i>
+                </a>
+                <a href="#">
+                    <i class="fas fa-trash-alt"></i>
+                </a>
+            </td>
+        </tr>
+        <tr>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td>
+                <a href="#">
+                    <i class="fas fa-plus"></i>
+                </a>
+            </td>
+        </tr>
+        </tbody>
+    </table>
+</div>
+</body>
+</html>
